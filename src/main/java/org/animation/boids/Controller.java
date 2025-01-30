38080f2ef9d49a41628457edd42de467771e4411
ATEZH <arthaxholli@gmail.com>
2025-01-30 23:30:38 +0100

    add pane and two buttons (start, pause) to view.fxml, declare them in Controller.java

--- src/main/java/org/animation/boids/Controller.java
+++ src/main/java/org/animation/boids/Controller.java
@@ -1,5 +1,29 @@
 package org.animation.boids;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
 
+public class Controller {
+    @FXML
+    Pane simulationPane;
+
+    @FXML
+    Button startButton;
+
+    @FXML
+    Button pauseButton;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void pause() {
+
+    }
 }
