f8cb4216e4873896f015932f0a81f3e1f6c4db99
ATEZH <arthaxholli@gmail.com>
2025-01-31 19:25:36 +0100

    add reset and resume button in fxml file, write the bodies of needed methods in Controller

--- src/main/java/org/animation/boids/Controller.java
+++ src/main/java/org/animation/boids/Controller.java
@@ -1,29 +1,50 @@
 package org.animation.boids;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import org.animation.boids.models.Boid;
+import org.animation.boids.models.Simulation;
+import org.animation.boids.views.BoidView;
 
 public class Controller {
     @FXML
     Pane simulationPane;
 
-    @FXML
-    Button startButton;
-
-    @FXML
-    Button pauseButton;
+    public Simulation simulation;
 
     @FXML
     public void initialize() {
-
+        simulationPane.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+        System.out.println(simulationPane.getWidth() + " " + simulationPane.getHeight());
     }
 
-    public void start() {
-
+    @FXML
+    public void resume() {
+        System.out.println("Start Simulation");
     }
 
+    @FXML
     public void pause() {
+        System.out.println("Pause Simulation");
+    }
 
+    @FXML
+    public void reset() {
+        System.out.println("Reset Simulation");
+        simulationPane.getChildren().clear();
+        create();
+    }
+
+    @FXML
+    private void create() {
+        System.out.println("Create Simulation");
+        simulation = new Simulation(simulationPane, 30);
+        for (Boid boid: simulation.getBoids()) {
+            new BoidView(boid, simulationPane);
+        }
     }
 }
