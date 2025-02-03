d9f1219dfd72c82477a1ab31a6b2d2e6b1feddf9
ATEZH <arthaxholli@gmail.com>
2025-02-03 23:22:42 +0100

    implement alignment (for a start)

--- src/main/java/org/animation/boids/models/Direction.java
+++ src/main/java/org/animation/boids/models/Direction.java
@@ -2,13 +2,15 @@ package org.animation.boids.models;
 
 import javafx.scene.layout.Pane;
 
+import java.util.ArrayList;
+
 public class Direction {
     private double dx;
     private double dy;
     static public double speed = 1;
-    static private double absoluteMaxDx = 1;
-    static private double absoluteMaxDy = 1;
-    static public double fieldOfView = 120;
+    static private double absoluteMaxDx = 2;
+    static private double absoluteMaxDy = 2;
+    static public double fieldOfView = 50;
     private double wallAvoidanceFactor = 0.02*speed;
     private double wallAvoidanceAlt = 0.003*speed;
 
@@ -27,16 +29,15 @@ public class Direction {
     }
 
     public void updateDirection(Boid thisBoid, Simulation simulation, Pane world) {
-        avoidWall(thisBoid, world);
+        alignment(thisBoid, simulation.getBoids(), fieldOfView*2);
+        avoidWall(thisBoid, world, fieldOfView*2);
     }
 
-    private void avoidWall(Boid thisBoid, Pane world) {
+    private void avoidWall(Boid thisBoid, Pane world, double fieldOfView) {
         if (thisBoid.position.getX() < fieldOfView) {
-            // (fieldOfView-thisBoid.position.getX())/fieldOfView -> distance from wall, ranges from 0 to 1
             dx += wallAvoidanceFactor*(fieldOfView-thisBoid.position.getX())/fieldOfView;
             if (dy > 0) dy += wallAvoidanceAlt; else dy -= wallAvoidanceAlt;
         } else if (thisBoid.position.getX() > world.getWidth() - fieldOfView) {
-            // (thisBoid.position.getX() - world.getWidth() + fieldOfView)/fieldOfView -> distance from wall, ranges from 0 to 1
             dx -= wallAvoidanceFactor*(thisBoid.position.getX() - world.getWidth() + fieldOfView)/fieldOfView;
             if (dy > 0) dy += wallAvoidanceAlt; else dy -= wallAvoidanceAlt;
         }
@@ -51,8 +52,22 @@ public class Direction {
         if (Math.abs(dy) > absoluteMaxDy) dy = absoluteMaxDy*Math.signum(dy);
     }
 
-    private void alignment() {
-
+    private void alignment(Boid thisBoid, ArrayList<Boid> boids, double fieldOfView) {
+        int localBoids = 0;
+        double totalDx = 0;
+        double totalDy = 0;
+        for (Boid boid: boids) {
+            double distance = measureDistance(thisBoid.position.getX(), boid.position.getX(),
+                    thisBoid.position.getY(), boid.position.getY());
+            if (distance < fieldOfView && distance > fieldOfView/3) {
+                ++localBoids;
+                totalDx += boid.direction.getDx();
+                totalDy += boid.direction.getDy();
+            }
+        }
+        if (localBoids == 0) return;
+        dx = 0.03*(totalDx/localBoids) + 0.97*dx;
+        dy = 0.03*(totalDy/localBoids) + 0.97*dy;
     }
 
     private void cohesion() {
@@ -61,5 +76,9 @@ public class Direction {
 
     private void separation() {
 
+    }
+
+    private double measureDistance(double x1, double x2, double y1, double y2) {
+        return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
     }
 }
